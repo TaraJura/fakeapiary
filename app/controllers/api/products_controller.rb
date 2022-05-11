4a7371da16f8c0b01443e02ca27282ce0c21b01e
Jiří Novák <jirkanovakneda11@seznam.cz>
2022-05-11 14:07:55 +0200

    autorization token

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class ProductsController < ApplicationController
     before_action :set_product, only: %i[ show edit update destroy ]
+    skip_before_action :verify_authenticity_token
 
     # GET /products or /products.json
     def index
