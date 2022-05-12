4566c797ca6ca51f2a18351ca7d447ee078c3061
Jan Vítek <jan.vitek@virtualmaster.com>
2022-05-12 07:48:24 +0000

    fix tracking number association to order

--- app/models/tracking_number.rb
+++ app/models/tracking_number.rb
@@ -1,5 +1,3 @@
 class TrackingNumber < ApplicationRecord
   belongs_to :order
-
-  validates :order_id, presence: true
 end
---
+++ db/migrate/20220512074344_add_order_association_to_tracking_number.rb
@@ -0,0 +1,5 @@
+class AddOrderAssociationToTrackingNumber < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :tracking_numbers, :order, null: false, foreign_key: true
+  end
+end
